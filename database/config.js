2e44e20afdcb94de66d84acfa3abb5ae5828393e
Miguel Correa <macc1712@gmail.com>
2024-02-16 17:06:30 -0500

    CORS y Express - Primer commit

---
+++ database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect( process.env.DB_CNN )
+
+        console.log('DB Online')
+
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error al iniciar la BD, ver logs')
+    }
+
+}
+
+module.exports = {
+    dbConnection
+}
