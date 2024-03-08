a78b2e545d07b860573f903ac971989b56a49d21
Miguel Correa <macc1712@gmail.com>
2024-03-08 08:48:52 -0500

    Avance 08-03 - BackEnd

--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -15,6 +15,6 @@ router.use( expressFileUpload() );
 
 router.put( '/:tipo/:id', [ validarJWT ], fileUpload )
 
-router.get( '/:tipo/:foto', [ validarJWT ], retornaImagen )
+router.get( '/:tipo/:foto', [], retornaImagen )
 
 module.exports = router
